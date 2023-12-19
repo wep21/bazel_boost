1b45151b604848051782446b31d3a9ca27c6e561
wep21 <daisuke.nishimatsu1021@gmail.com>
2023-12-20 02:57:47 +0900

    feat: initial commit
    Signed-off-by: wep21 <daisuke.nishimatsu1021@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+namespace asio = boost::asio;
+using asio::ip::tcp;
+
+int main()
+{
+    asio::io_service io_service;
+    tcp::socket socket(io_service);
+
+    boost::system::error_code error;
+    socket.connect(tcp::endpoint(asio::ip::address::from_string("127.0.0.1"), 31400), error);
+
+    if (error) {
+        std::cout << "connect failed : " << error.message() << std::endl;
+    }
+    else {
+        std::cout << "connected" << std::endl;
+    }
+}
